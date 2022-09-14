51513784405515356e6f4fdfdbde874eb7c4c590
roymutua <roy.ngure@gmail.com>
2022-09-14 07:39:41 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - Retuns 1 if char is lowercase character
+ * else, return 0
+ *
+ * @c: The character in ASCII code
+ *
+ * Return: 1 for lowercase. 0 for the rest.
+ */
+int _islower(int i)
+{
+	if (i >= 'a' && i <= 'z')
+		return (1);
+	else
+		 return (0);
+}
