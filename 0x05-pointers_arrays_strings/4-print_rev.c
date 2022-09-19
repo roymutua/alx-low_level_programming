98be648129b8281c9ef785099786b4a436b1f4df
roymutua <roy.ngure@gmail.com>
2022-09-19 18:27:58 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints string
+ * @s: input string
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
