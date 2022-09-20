7865bbdd709b7360172f82e029657e22164fc640
roymutua <roy.ngure@gmail.com>
2022-09-20 05:53:44 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,16 +5,15 @@
  */
 void puts2(char *str)
 {
-	int i, j = 0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 		i++;
-	}
-	while (j < i)
-	{
-		_putchar(str[j]);
-		j = j + 2;
 	}
 	_putchar('\n');
 }
