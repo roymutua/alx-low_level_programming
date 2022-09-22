a3e891f9e7e8a722775f1d617abc901808c2b39e
roymutua <roy.ngure@gmail.com>
2022-09-22 09:38:18 +0000

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,8 +10,8 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
@@ -19,7 +19,6 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 
 	i = i;
-
 	while (i < n)
 	{
 		dest[i] = '\0';
