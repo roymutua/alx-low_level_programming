2826a87defc3719d2032b0d383c092905ee4e2ec
roymutua <roy.ngure@gmail.com>
2022-09-22 10:18:12 +0000

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,17 +7,14 @@
  */
 char *string_toupper(char *str)
 {
-	int i;
+	int index = 0;
 
-	i = 0;
-
-	while (str[i] != '\0')
+	while (str[index])
 	{
-		if (str[i] >= 97 && str[i] <= 122)
-		{
-			str[i] = str[i] - 32;
-		}
-		i++
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
 	}
 	return (str);
 }
