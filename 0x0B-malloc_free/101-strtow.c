1410f0a1bba1a78bf84ea1efdec49b81d9560ac5
roymutua <roy.ngure@gmail.com>
2022-10-05 18:58:08 +0000

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,54 +1,65 @@
 #include "main.h"
+#include <stdlib.h>
 /**
- * len - returns length of str
- * @str: string counted
+ * ch_free_grid - frees a 2 dimensional array.
+ * @grid: multidimensional array of char.
+ * @height: height of the array.
  *
- * Return: returns the length
+ * Return: no return
  */
-int len(char *str)
+void ch_free_grid(char **grid, unsigned int height)
 {
-	int len = 0;
-
-	if (str != NULL)
+	if (grid != NULL && height != 0)
 	{
-		while (str[len])
-			len++;
+		for (; height > 0; height--)
+			free(grid[height]);
+		free(grid[height]);
+		free(grid);
 	}
-	return (len);
 }
 /**
- * argstostr - a function that concatenates all the arguments of your program
- * @ac: count of args passed to the function
+ * strtow - splits a string into words.
+ * @str: string.
  *
- * Return: pointer to the new string
+ * Return: pointer of an array of integers
  */
-char *argstostr(int ac, char **av)
+char **strtow(char *str)
 {
-	char *new_string = NULL;
-	int k = 0, i = ac, j, sum = 0, temp = 0;
+	char **aout;
+	unsigned int c, height, i, j, a1;
 
-	if (ac == 0 || av == NULL)
+	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	while (ac--)
-		sum += (len(av[ac]) + 1);
-	new_string = (char *) malloc(sum + 1);
-
-	if (new_string != NULL)
+	for (c = height = 0; str[c] != '\0'; c++)
+		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			height++;
+	aout = malloc((height + 1) * sizeof(char *));
+	if (aout == NULL || height == 0)
 	{
-		while (k < i)
+		free(aout);
+		return (NULL);
+	}
+	for (i = a1 = 0; i < height; i++)
+	{
+		for (c = a1; str[c] != '\0'; c++)
 		{
-			for (j = 0; av[k][j] != '\0'; j++)
-				new_string[j + temp] = av[k][j];
-			new_string[temp + j] = '\n';
-			temp += (j + 1);
-			k++;
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			{
+				aout[i] = malloc((c - a1 + 2) * sizeof(char));
+				if (aout[i] == NULL)
+				{
+					ch_free_grid(aout, i);
+					return (NULL);
+				}
+				break;
+			}
 		}
-		new_string[temp] = '\0';
+		for (j = 0; a1 <= c; a1++, j++)
+			aout[i][j] = str[a1];
+		aout[i][j] = '\0';
 	}
-	else
-	{
-		return (NULL);
-	}
-	return (new_string);
+	aout[i] = NULL;
+	return (aout);
 }
