2c8c7b8a4f4a53d2464b8ac33e39032ef53d4816
roymutua <roy.ngure@gmail.com>
2022-10-13 10:10:45 +0000

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * print_name - print a name
+ * @name: String to print
+ * @f: Pointer to function
+ *
+ * Return: function that prints name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+	{
+		(*f)(name);
+	}
+}
