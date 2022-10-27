4706afbf536f07e183cb254401a7a27653175f75
roymutua <roy.ngure@gmail.com>
2022-10-27 19:17:36 +0000

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -25,18 +25,11 @@ size_t looped_listint_count(listint_t *head)
 		if (ahead == here)
 		{
 			ahead = head;
+
 			while (ahead != here)
 			{
-				nodes++;
 				ahead = ahead->next;
 				here = here->next;
-			}
-
-			ahead = ahead->next;
-			while (ahead != here)
-			{
-				nodes++;
-				ahead = ahead->next;
 			}
 
 			return (nodes);
@@ -44,45 +37,5 @@ size_t looped_listint_count(listint_t *head)
 		ahead = ahead->next;
 		here = (here->next)->next;
 	}
-
 	return (0);
-}
-
-/**
- * free_listint_safe - Frees a listint_t list.
- * @h: A pointer to the address of the head of the listint_t.
- *
- * Return: void.
- */
-size_t free_listint_safe(listint_t **h)
-{
-	listint_t *next;
-	size_t nodes, index;
-
-	nodes = looped_listint_count(*h);
-
-	if (nodes == 0)
-	{
-		for (; h != NULL && *h != NULL; nodes++)
-		{
-			next = (*h)->next;
-			free(*h);
-			*h = next;
-		}
-	}
-
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			next = (*h)->next;
-			free(*h);
-			*h = next;
-		}
-
-		*h = NULL;
-	}
-
-	h = NULL;
-	return (nodes);
 }
