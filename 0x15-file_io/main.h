a89e6c5439abe1503a6071b1ef02f7cb75cec262
roymutua <roy.ngure@gmail.com>
2022-11-02 12:50:06 +0000

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,6 @@
 #ifndef main_h
 #define main_h
 
-/**
- * Desc: Header file containing prototypes for all functions written in the 0x15 C - File I/O directory.
- */
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -15,6 +11,7 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif /* main_h */
